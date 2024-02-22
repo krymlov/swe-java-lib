8c3ecc7d97cfc1416dab377399f97970f2da04bc
Yuriy <yuriy.krymlov@gmail.com>
2024-02-22 20:12:49 +0200

    2.10.03j3d

--- src/main/java/org/swisseph/SwephNative.java
+++ src/main/java/org/swisseph/SwephNative.java
@@ -1,38 +1,38 @@
 /*
-* Copyright (C) By the Author
-* Author    Yura Krymlov
-* Created   2020-05
-*/
+ * Copyright (C) By the Author
+ * Author    Yura Krymlov
+ * Created   2020-05
+ */
 
 package org.swisseph;
 
 
 import swisseph.SwephExp;
 
+import static swisseph.SwephExp.SWISSEPH_LIBRARY_NAME;
+
 /**
  * @author Yura Krymlov
  * @version 1.1, 2022-05
  */
 public class SwephNative implements ISwissEph {
-
-    public static final String SWISSEPH_LIBRARY_NAME = "swe-2.10.03j3c";
     protected static final SwephExp swephExp = new SwephExp(SWISSEPH_LIBRARY_NAME);
 
     protected String ephe_path;
     protected boolean topo_set;
     protected double geo_alt;
-    
+
     public SwephNative(String ephePath) {
         swe_set_ephe_path(ephePath);
     }
 
     // ----------------------------------------------------------------------
-    
+
     @Override
     public boolean swe_set_topo() {
         return topo_set;
     }
-    
+
     @Override
     public double swe_get_geo_alt() {
         return geo_alt;
